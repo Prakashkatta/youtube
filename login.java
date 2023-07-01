a4b820644c5d61082e7bab9891318dea78801d00
prakash <root@ip-172-31-47-180.ap-south-1.compute.internal>
2023-07-01 06:49:48 +0000

    changes code

--- login.java
+++ login.java
@@ -1,12 +1,16 @@
 class Main {
 
-  public static void main(String[] args) {
+public static void main(String[] args) {
     
-    int first = 10;
-    int second = 20;
+    int first = 20;
+    int second = 30;
 
     // add two numbers
     int sum = first + second;
     System.out.println(first + " + " + second + " = "  + sum);
   }
+
+
+
+
 }
